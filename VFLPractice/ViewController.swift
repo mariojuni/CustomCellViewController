4449943a9d02fadf9c47277d317ad4b465714014
mario.juni <mario@veritaspay.com>
2019-06-03 16:02:56 +0800

    Initial Commit

---
+++ VFLPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VFLPractice
+//
+//  Created by Mario Juni on 03/06/2019.
+//  Copyright © 2019 VeritasPay Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
